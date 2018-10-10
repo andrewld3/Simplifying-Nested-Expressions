df46ae37fb11f830094faf1906d799ae80d5fff3
andrew <adriscol@oswego.edu>
2018-10-10 11:13:09 -0400

    Added And functionality Added Or funcationality Not function WIP

--- src/MP2.clj
+++ src/MP2.clj
@@ -1,10 +1,36 @@
 (ns MP2)
 
 ;;TODO; Write And Function
+(defn andExpression [expList]
+  (cond
+    (some false? expList) false
+    (some symbol? (rest expList))
+      (cond
+        (> 2 (count (remove true? (rest expList)))) (last (remove true? (rest expList)))
+        (<= 2 (count (remove true? (rest expList)))) (remove true? expList)
+        )
+    )
+  )
 
 ;;TODO: Write Or Function
+(defn orExpression [expList]
+  (cond
+    (some true? expList) true
+    (some symbol? (rest expList))
+      (cond
+        (<= 2 (count (remove false? (rest expList)))) (remove false? expList)
+        (> 2 (count (remove false? (rest expList)))) (last (remove false? (rest expList)))
+        )
+    :else false
+    )
+  )
 
 ;;TODO: Write Not Function
+(defn notExpression [explist]
+  (cond
+    (== (last expList) true) false
+    (== (last expList) false) true
+    ))
 
 ;;TODO: Write Logic
 
